6a5b214933916e28ea771c3dc5e9528f0d7804b4
AbelC27 <abelcaluseri@yahoo.com>
2024-11-01 13:22:47 +0200

    1.First commit-project-setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        data:"Hello from signup",
+    });
+};
+
+export const login = async (req, res) => {
+    res.json({
+        data:"Hello from login",
+    });
+};
+
+export const logout = async (req, res) => {
+    res.json({
+        data:"Hello from logout",
+    });
+};
